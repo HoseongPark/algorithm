ad35e7bd016662fa044e74aa169ad4e15b05d747
bluesky <ghtjd5689@gmail.com>
2020-04-17 23:48:12 +0900

    Heap Commit

--- src/datastructure/Heap/heap.py
+++ src/datastructure/Heap/heap.py
@@ -17,7 +17,8 @@ class Heap:
         self.heap.append(data)
 
     
-    def _moving(self, currentIndex):
+    # Current Index Value 와 Parent Index Value 의 값을 비교해서 바꿔야하는지 확인하는 Method
+    def _movingiUp(self, currentIndex): 
         parentIndex = currentIndex // 2
 
         if currentIndex <= 1: # Root Index
@@ -39,12 +40,70 @@ class Heap:
 
         currentIndex = len(self.heap) - 1
 
-        while self._moving(currentIndex):
+        while self._movingiUp(currentIndex):
             parentIndex = currentIndex // 2
             self.heap[currentIndex], self.heap[parentIndex] = self.heap[parentIndex], self.heap[currentIndex]
             currentIndex = parentIndex
 
         return True
+
+    
+    def _movingDown(self, currentIndex):
+        leftChildIndex = currentIndex * 2
+        rightChildIndex = currentIndex * 2 + 1
+
+        if leftChildIndex >= len(self.heap): # Left Child Data가 없을때.
+            return False
+
+        elif rightChildIndex >= len(self.heap): # Right Child Data가 없을때 (Left Child Data 만 있을때.)
+            if self.heap[leftChildIndex] > self.heap[currentIndex]:
+                return True
+            else:
+                return False
+
+        else: # Left Child Data 와 Right Child Data 둘다 있을때.
+            if self.heap[leftChildIndex] > self.heap[rightChildIndex]: # Left Child Data > Right Child Data
+                if self.heap[leftChildIndex] > self.heap[currentIndex]:
+                    return True
+                else:
+                    return False
+
+            else: # Right Child Data > Left Child Data
+                if self.heap[rightChildIndex] > self.heap[currentIndex]:
+                    return True
+                else:
+                    return False
+
+
+    def pop(self):
+        if len(self.heap) <= 1:
+            return None
+
+        popData = self.heap[1]
+        self.heap[1] = self.heap[-1] # 마지막 Data를 Root Data로 바꿈.
+        del self.heap[-1] # 마지막 Data 삭제.
+        
+        currentIndex = 1
+        while self._movingDown(currentIndex):
+            leftChildIndex = currentIndex * 2
+            rightChildIndex = currentIndex * 2 + 1
+
+            if rightChildIndex >= len(self.heap):
+                if self.heap[leftChildIndex] > self.heap[currentIndex]:
+                    self.heap[leftChildIndex], self.heap[currentIndex] = self.heap[currentIndex], self.heap[leftChildIndex]
+                    currentIndex = leftChildIndex
+                    
+            else:
+                if self.heap[leftChildIndex] > self.heap[rightChildIndex]:
+                    if self.heap[leftChildIndex] > self.heap[currentIndex]:
+                        self.heap[leftChildIndex], self.heap[currentIndex] = self.heap[currentIndex], self.heap[leftChildIndex]
+                        currentIndex = leftChildIndex
+                else:
+                    if self.heap[rightChildIndex] > self.heap[currentIndex]:
+                        self.heap[rightChildIndex], self.heap[currentIndex] = self.heap[currentIndex], self.heap[rightChildIndex]
+                        currentIndex = rightChildIndex
+
+        return popData
 
 
 if __name__ == "__main__":
@@ -55,5 +114,14 @@ if __name__ == "__main__":
     heap.insert(5)
     heap.insert(4)
     heap.insert(20)
+    heap.insert(30)
+
+    print(heap.pop())
+    print(heap.pop())
+    print(heap.pop())
+    print(heap.pop())
+    print(heap.pop())
+    print(heap.pop())
+    print(heap.pop())
 
     print(heap.heap)
